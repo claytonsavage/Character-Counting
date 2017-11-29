7a36408d96e19d5a260d4838d854181cf602270f
Clayton Savage <clytnsavage@gmail.com>
2017-11-29 14:55:06 -0800

    removed comments

--- countLetters.js
+++ countLetters.js
@@ -1,25 +1,15 @@
-
-
-
 function countLetters(strings) {
-  //var strings = strings.split("");
-  // 1. Initialize a return value
   var array = {};
-  // 2. Loop over the input
   for (var i = 0; i < strings.length; i++) {
     var string = strings[i];
     if (string !== ' ') {
-      // Don't worry about it
       if (array[strings[i]] === undefined) {
         array[strings[i]] = 0;
       }
-      // 3. Inside the loop, modify the return value
       array[strings[i]] += 1;
     }
   }
-  // 4. Return the return value
   return array;
 }
-
 
 console.log(countLetters('this is my string lighthouse labs'));
