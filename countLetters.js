f8b8165053f41a7c5d559b5fe6d2e3d875adf2ea
claytonsavage <clytnsavage@gmail.com>
2017-11-29 20:05:09 +0000

    added countLetters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+
+
+
+function countLetters(strings) {
+  var strings = strings.split("");
+  // 1. Initialize a return value
+  var array = {};
+  // 2. Loop over the input
+  for (var i = 0; i < strings.length; i++) {
+    var string = strings[i];
+    if (string === ' ') {
+      // Don't worry about it
+      continue;
+    }
+    if (array[strings[i]] === undefined) {
+      array[strings[i]] = 0;
+    }
+    // 3. Inside the loop, modify the return value
+    array[strings[i]] += 1;
+  }
+  // 4. Return the return value
+  return array;
+}
+
+
+console.log(countLetters('this is my string lighthouse labs'));
